16a82a0608b8f237e33b2e987f732a53cd46ff89
kdk <zoaustkd@naver.com>
2023-04-27 12:18:45 +0900

    2021 월드비전 환아

---
+++ images_hwanAh/js/newbird_k.js
@@ -0,0 +1,29 @@
+$(function(){
+    // $(".sec04.sec").css("height",$(window).outerHeight() * 4);
+
+
+    // var i = 0;
+    // var j = 0;
+    // $(window).scroll(function(){
+    //     if($(window).scrollTop() >= $(".sec04.sec").offset().top && $(window).scrollTop() + $(window).outerHeight() <= $(".sec04.sec").offset().top + $(".sec04.sec").outerHeight()){
+    //         $("#hwanAh_wrap .sec04 .txtWrp").css("top",$(window).scrollTop() - $(".sec04.sec").offset().top);
+    //         $("#hwanAh_wrap .sec04 .imgWrp").css("top",$(window).scrollTop() - $(".sec04.sec").offset().top);
+    //     };
+
+    //     if($(window).scrollTop() >= $(".sec04.sec").offset().top + $(window).outerHeight() && $(window).scrollTop() <= $(".sec04.sec").offset().top + $(window).outerHeight() * 2){
+    //         i = $(window).scrollTop() - $(".sec04.sec").offset().top - $(window).outerHeight();
+    //         j = $(window).outerHeight();
+    //         $("#hwanAh_wrap .sec04 .imgWrp img").eq(1).css("opacity",1 - (i / j));
+    //     } else if($(window).scrollTop() > $(".sec04.sec").offset().top + $(window).outerHeight() * 2){
+    //         $("#hwanAh_wrap .sec04 .imgWrp img").eq(1).css("opacity",0);
+    //     } else if($(window).scrollTop() < $(".sec04.sec").offset().top + $(window).outerHeight()){
+    //         $("#hwanAh_wrap .sec04 .imgWrp img").eq(1).css("opacity",1);
+    //     }
+    // });
+
+
+    
+}); //funciton end
+
+
+
